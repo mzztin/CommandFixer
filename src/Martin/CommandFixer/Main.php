72707f24ded58c3e2642319105208bf191c2c8f3
martin @ exze <exze.real@gmail.com>
2020-08-09 11:48:01 +0200

    Fixing some code

--- src/Martin/CommandFixer/Main.php
+++ src/Martin/CommandFixer/Main.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Martin\CommandFixer;
 
 use pocketmine\event\Listener;
@@ -10,14 +8,17 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener
 {
-    public function onEnable()
-    {
+    public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onCommandEvent(CommandEvent  $ev): void {
         $args = explode(" ", $ev->getCommand());
         $command = strtolower(array_shift($args));
+        if (!$args) {
+            $ev->setCommand($command);
+            return;
+        }
         $ev->setCommand($command . " " . join(" ", $args));
     }
 }
